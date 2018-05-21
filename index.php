66515129d4205e4f8fd750fd4b0dd0f689d40de9
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-21 15:18:21 -0300

    Injeção de duas querys e deleletar tabela completa

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ $sql = "SELECT * FROM posts WHERE autor = '$autor'";
 
 $sql = "SELECT * FROM posts WHERE autor = '' or 1=1'";
 
+// '; DROP TABLE posts;
 
-
+$sql = "SELECT * FROM posts WHERE autor = ''; DROP TABLE posts;'";
 
 ?>
